2f1da9d7bdbd4318650fff18af7feaf0ce7d07a9
eliheist <eliyang256@outlook.com>
2023-05-19 23:30:06 +0300

    re-add is_active field to guest model

--- guests/models.py
+++ guests/models.py
@@ -5,7 +5,7 @@ class Guest(models.Model):
    name = models.CharField(max_length=50)
    phone = models.CharField(max_length=50, unique=True)
    email = models.EmailField(blank=True, null=True)
-   # is_active = models.BooleanField(default=True)
+   is_active = models.BooleanField(default=True)
 
    def __str__(self):
       return self.name
